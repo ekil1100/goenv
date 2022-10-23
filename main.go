0d8630e862ac05c56e64e39746a9eb1b855be64a
Like <like@ekil.sh>
2022-10-23 23:37:12 +0800

    .

--- main.go
+++ main.go
@@ -1,144 +1,144 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
 type Env struct {
-    Kv   map[string]string // key value store
-    Keys []string          // to keep the order of the keys
+	Kv   map[string]string // key value store
+	Keys []string          // to keep the order of the keys
 }
 
 func Filter(list []string, fn func(string) bool) (result []string) {
-    for _, item := range list {
-        if fn(item) {
-            result = append(result, item)
-        }
-    }
-    return result
+	for _, item := range list {
+		if fn(item) {
+			result = append(result, item)
+		}
+	}
+	return result
 }
 
 func (env *Env) String() string {
-    var str string
-    for _, key := range env.Keys {
-        value := env.Kv[key]
-        str += fmt.Sprintf("%s=%s\n", key, value)
-    }
-    return str
+	var str string
+	for _, key := range env.Keys {
+		value := env.Kv[key]
+		str += fmt.Sprintf("%s=%s\n", key, value)
+	}
+	return str
 }
 
 func (env *Env) Add(key, value string) {
-    env.Keys = Filter(env.Keys, func(k string) bool {
-        return k != key
-    })
-    env.Keys = append(env.Keys, key)
-    env.Kv[key] = value
+	env.Keys = Filter(env.Keys, func(k string) bool {
+		return k != key
+	})
+	env.Keys = append(env.Keys, key)
+	env.Kv[key] = value
 }
 
 func (env *Env) Parser(data []byte) {
-    for lineNumber, line := range strings.Split(string(data), "\n") {
-        // trim spaces
-        line = strings.TrimSpace(line)
+	for lineNumber, line := range strings.Split(string(data), "\n") {
+		// trim spaces
+		line = strings.TrimSpace(line)
 
-        // skip # comments
-        if strings.HasPrefix(line, "#") {
-            continue
-        }
+		// skip # comments
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
 
-        // skip empty lines
-        if line == "" {
-            continue
-        }
+		// skip empty lines
+		if line == "" {
+			continue
+		}
 
-        // panic if no = sign, invalid syntax
-        if !strings.Contains(line, "=") {
-            panic(fmt.Sprintf("Line %d: `%s` is not a valid environment variable\n", lineNumber+1, line))
-        }
+		// panic if no = sign, invalid syntax
+		if !strings.Contains(line, "=") {
+			panic(fmt.Sprintf("Line %d: `%s` is not a valid environment variable\n", lineNumber+1, line))
+		}
 
-        // split key and value and store into a map
-        parts := strings.SplitN(line, "=", 2)
-        key, value := strings.TrimSpace(parts[0]), strings.Trim(parts[1], `" `)
-        env.Add(key, value)
-    }
+		// split key and value and store into a map
+		parts := strings.SplitN(line, "=", 2)
+		key, value := strings.TrimSpace(parts[0]), strings.Trim(parts[1], `" `)
+		env.Add(key, value)
+	}
 
-    // solve nested values
-    for _, key := range env.Keys {
-        value := env.Kv[key]
-        env.Add(key, env.SolveNestedValues(value))
-    }
+	// solve nested values
+	for _, key := range env.Keys {
+		value := env.Kv[key]
+		env.Add(key, env.SolveNestedValues(value))
+	}
 }
 
 func (env *Env) SolveNestedValues(value string) string {
-    for i := 0; i < len(value); i++ {
-        if value[i] == '$' {
-            end := strings.Index(value[i:], "}") + i
-            parameter := value[i+2 : end]
-            spitedParameter := strings.Split(parameter, ":")
+	for i := 0; i < len(value); i++ {
+		if value[i] == '$' {
+			end := strings.Index(value[i:], "}") + i
+			parameter := value[i+2 : end]
+			spitedParameter := strings.Split(parameter, ":")
 
-            var key, defaultValue string
-            var replace, hasDefault bool
-            var replacer string
+			var key, defaultValue string
+			var replace, hasDefault bool
+			var replacer string
 
-            if len(spitedParameter) > 1 {
-                hasDefault = true
-                key = spitedParameter[0]
-                if strings.HasPrefix(spitedParameter[1], "=") {
-                    replace = true
-                    defaultValue = spitedParameter[1][1:]
-                } else if strings.HasPrefix(spitedParameter[1], "-") {
-                    defaultValue = spitedParameter[1][1:]
-                } else {
-                    defaultValue = spitedParameter[1]
-                }
-            } else {
-                key = spitedParameter[0]
-            }
+			if len(spitedParameter) > 1 {
+				hasDefault = true
+				key = spitedParameter[0]
+				if strings.HasPrefix(spitedParameter[1], "=") {
+					replace = true
+					defaultValue = spitedParameter[1][1:]
+				} else if strings.HasPrefix(spitedParameter[1], "-") {
+					defaultValue = spitedParameter[1][1:]
+				} else {
+					defaultValue = spitedParameter[1]
+				}
+			} else {
+				key = spitedParameter[0]
+			}
 
-            if hasDefault {
-                replacer = defaultValue
-            } else {
-                var ok bool
-                replacer, ok = env.Kv[key]
-                if !ok {
-                    panic(fmt.Sprintf("Key %s not found in `%s`", key, value))
-                }
-            }
+			if hasDefault {
+				replacer = defaultValue
+			} else {
+				var ok bool
+				replacer, ok = env.Kv[key]
+				if !ok {
+					panic(fmt.Sprintf("Key %s not found in `%s`", key, value))
+				}
+			}
 
-            if replace {
-                env.Add(key, replacer)
-            }
+			if replace {
+				env.Add(key, replacer)
+			}
 
-            target := "${" + parameter + "}"
-            value = strings.Replace(value, target, replacer, 1)
-        }
-    }
-    return value
+			target := "${" + parameter + "}"
+			value = strings.Replace(value, target, replacer, 1)
+		}
+	}
+	return value
 }
 
 func Load() {
-    data, err := os.ReadFile(".env")
-    if err != nil {
-        fmt.Println(err)
-    }
-    env := &Env{Kv: make(map[string]string), Keys: make([]string, 0)}
-    env.Parser(data)
-    fmt.Println("Loading environment values from .env file:")
-    fmt.Println(env)
-    for key, value := range env.Kv {
-        err := os.Setenv(key, value)
-        if err != nil {
-            panic(err)
-        }
-    }
+	data, err := os.ReadFile(".env")
+	if err != nil {
+		fmt.Println(err)
+	}
+	env := &Env{Kv: make(map[string]string), Keys: make([]string, 0)}
+	env.Parser(data)
+	fmt.Println("Loading environment values from .env file:")
+	fmt.Println(env)
+	for key, value := range env.Kv {
+		err := os.Setenv(key, value)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func init() {
-    Load()
+	Load()
 }
 
 func main() {
-    fmt.Println("In main program:")
-    fmt.Println(os.Getenv("DATABASE_URL"))
+	fmt.Println("In main program:")
+	fmt.Println(os.Getenv("DATABASE_URL"))
 }
